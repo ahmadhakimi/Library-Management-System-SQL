dc2ce214f2c3909241102517ed2bebf74e708f70
AHMAD_HAKIMI AHMAD_RASIDI <hakimihakimi611@gmail.com>
2024-01-14 10:01:24 +0800

    add database publisher and book

---
+++ insert-publisher-lms.sql
@@ -0,0 +1,11 @@
+INSERT INTO PUBLISHER VALUES (100000, 'Kiyosaki', 'Kuantan', 'robertkiyo@gmail.com' ); 
+INSERT INTO PUBLISHER VALUES (100001, 'Tolkien', 'Selangor', 'tolkien@gmail.com' ); 
+INSERT INTO PUBLISHER VALUES (100002, 'J.K. Rowling', 'Sabah', 'Rowling@gmail.com' ); 
+INSERT INTO PUBLISHER VALUES (100003, 'Adnin', 'Kota Bahru', 'dakwahsentap@gmail.com' ); 
+INSERT INTO PUBLISHER VALUES (100004, 'Abdul Latip', 'London', 'abdullatiptalib@gmail.com' ); 
+INSERT INTO PUBLISHER VALUES (100005, 'Aenid Abdullah', 'Vancouver', 'aenidabdullah@gmail.com' ); 
+INSERT INTO PUBLISHER VALUES (100006, 'Prof. Hamka', 'Tokyo', 'Hamkalegend@gmail.com' ); 
+INSERT INTO PUBLISHER VALUES (100007, 'Ayman Wong', 'Seoul', 'aymanwong@gmail.com' ); 
+INSERT INTO PUBLISHER VALUES (100008, 'Veronica', 'Guangzhou', 'vroth@gmail.com' ); 
+INSERT INTO PUBLISHER VALUES (100009, 'Jamil', 'St. Petersburg', 'Jamil@gmail.com' ); 
+INSERT INTO PUBLISHER VALUES (100010, 'S. Hawking', 'Melaka', 'eipstein@gmail.com' ); 
