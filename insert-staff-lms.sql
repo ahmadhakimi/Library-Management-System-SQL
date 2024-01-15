dc97eda6cb2512006654da2f96fad81548e5fa17
AHMAD_HAKIMI AHMAD_RASIDI <hakimihakimi611@gmail.com>
2024-01-15 21:15:28 +0800

    insert staffs more

--- insert-staff-lms.sql
+++ insert-staff-lms.sql
@@ -3,3 +3,10 @@ INSERT INTO STAFF VALUES (203000, 'Orichi Doppo', 'Manager', 'southtiger@gmail.c
 INSERT INTO STAFF VALUES (204000, 'Pickle', 'Assistant', 'relics@gmail.com', '014-8592811'); 
 INSERT INTO STAFF VALUES (205000, 'Hanayama Kaoru', 'Assistant', 'YuchiGumi@gmail.com', '013-75196117'); 
 INSERT INTO STAFF VALUES (206000, 'Jack Hanma', 'Assistant', 'jack@gmail.com', '019-71987161'); 
+INSERT INTO STAFF VALUES (208000, 'Retsu Kaioh', 'Assistant', 'retsu@gmail.com', '014-56789012');
+
+INSERT INTO STAFF VALUES (209000, 'Kozue Matsumoto', 'Assistant', 'kozue@gmail.com', '016-12345678');
+
+INSERT INTO STAFF VALUES (210000, 'Motobe Izo', 'Supervisor', 'motobe@gmail.com', '017-98765432');
+
+INSERT INTO STAFF VALUES (211000, 'Kureha Shinogi', 'Assistant', 'kureha@gmail.com', '018-87654321');
