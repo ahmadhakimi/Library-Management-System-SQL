ac7faeefed19d4891c3034730fa739ed12d64858
AHMAD_HAKIMI AHMAD_RASIDI <hakimihakimi611@gmail.com>
2024-01-16 11:10:32 +0800

    Undo the changes

--- updatememberdetailsprocedureblock.sql
+++ updatememberdetailsprocedureblock.sql
@@ -2,36 +2,16 @@ SET SERVEROUTPUT ON;
 SET VERIFY OFF;
 
 DECLARE
-    v_MemberID NUMBER := &member_id;
-    v_NewEmail VARCHAR2(30);
-    v_NewAddress VARCHAR2(20);
-    v_NewPhone VARCHAR2(12);
+    v_MemberID NUMBER := &member_id; 
+    v_NewEmail VARCHAR2(30) := '&new_email';
+    v_NewAddress VARCHAR2(20) := '&new_address'; 
+    v_NewPhone VARCHAR2(12) := '&new_phone'; 
 BEGIN
-
-    v_NewEmail := '&new_email';
-    v_NewAddress := '&new_address';
-    v_NewPhone := '&new_phone';
-
-     IF v_NewEmail = 'NULL' THEN
-        SELECT Email INTO v_NewEmail FROM MEMBER WHERE MemberID = v_MemberID;
-    END IF;
-
-    IF v_NewAddress = 'NULL' THEN
-        SELECT Address INTO v_NewAddress FROM MEMBER WHERE MemberID = v_MemberID;
-    END IF;
-
-    IF v_NewPhone = 'NULL' THEN
-        SELECT Phone INTO v_NewPhone FROM MEMBER WHERE MemberID = v_MemberID;
-    END IF;
-
-    
-    IF v_NewEmail IS NOT NULL OR v_NewAddress IS NOT NULL OR v_NewPhone IS NOT NULL THEN
-        UpdateMemberDetails(
-            p_member_id => v_MemberID,
-            p_new_email => v_NewEmail,
-            p_new_address => v_NewAddress,
-            p_new_phone => v_NewPhone
-        );
-    END IF;
+    UpdateMemberDetails(
+        p_member_id => v_MemberID,
+        p_new_email => v_NewEmail,
+        p_new_address => v_NewAddress,
+        p_new_phone => v_NewPhone
+    );
 END;
 /
